5633ce39f475a4647e2e2497b03aaf1a9b7a4e2c
wang-jie-2020 <421004034@qq.com>
2020-09-04 15:39:58 +0800

    去掉md 去掉仓储注入 修改domain

--- AbpDtoGenerator.LGFeature/CodeGenExtension.cs
+++ AbpDtoGenerator.LGFeature/CodeGenExtension.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Security.Policy;
 using System.Text;
 using AbpDtoGenerator;
@@ -10,6 +12,9 @@ using AbpDtoGenerator.Enums;
 using AbpDtoGenerator.GeneratorModels;
 using AbpDtoGenerator.Models;
 using AbpDtoGenerator.ViewModels;
+using Microsoft.CodeAnalysis;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AbpDtoGenerator.LGFeature
 {
@@ -28,7 +33,7 @@ namespace AbpDtoGenerator.LGFeature
                 {
                     MainWindowsOptionCfg = Global.Option
                 };
-                GeneratorCodeReplaceInfo replaceInfo = GeneratorCodeService.Create(Global.SolutionInfo, Global.Option, Global.Entity);
+                GeneratorCodeReplaceInfo replaceInfo = GeneratorCodeServiceExtension.Create(Global.SolutionInfo, Global.Option, Global.Entity);
                 ServerModel serverModel = new ServerModel
                 {
                     ReplaceInfo = replaceInfo,
@@ -74,12 +79,79 @@ namespace AbpDtoGenerator.LGFeature
                         codeTemplateInfo2.BuildPath.CreateFile(codeTemplateInfo2.BuildCode);
                     }
                 }
+
+                //多语言
+                if (Global.Option.UseDomainAuthorizeCode)
+                {
+                    var sourceCode = string.Empty;
+                    var desFile = string.Empty;
+
+                    foreach (CodeTemplateInfo codeTemplateInfo in list)
+                    {
+                        if (!File.Exists(codeTemplateInfo.BuildPath) || Global.Option.IsOverrideFile)
+                        {
+                            if (codeTemplateInfo.Path.EndsWith("Localization-zh.txt"))
+                            {
+                                sourceCode = codeTemplateInfo.BuildCode;
+                                desFile = Path.Combine(Global.SolutionInfo.Core.BasePath, "Localization", "SourceFiles",
+                                    "json", "YoyoCmsTemplate-zh-Hans.json");
+                            }
+                            else if (codeTemplateInfo.Path.EndsWith("Localization-en.txt"))
+                            {
+                                sourceCode = codeTemplateInfo.BuildCode;
+                                desFile = Path.Combine(Global.SolutionInfo.Core.BasePath, "Localization", "SourceFiles",
+                                    "json", "YoyoCmsTemplate.json");
+                            }
+
+                            if (!string.IsNullOrEmpty(sourceCode) && File.Exists(desFile))
+                            {
+                                var dicSource = JsonConvert.DeserializeObject<Dictionary<string, string>>(sourceCode);
+
+                                //JsonTextReader jsonTextReader = new JsonTextReader(new StringReader(File.ReadAllText(desFile, Encoding.UTF8)));
+                                //var json = new JsonSerializer().Deserialize(jsonTextReader);
+                                //var localization = JsonConvert.DeserializeObject<Localization>(json.ToString());
+
+                                var localization = JsonConvert.DeserializeObject<Localization>(File.ReadAllText(desFile, Encoding.UTF8));
+                                var dicDes = localization.Texts;
+
+                                foreach (var item in dicSource)
+                                {
+                                    if (dicDes.ContainsKey(item.Key))
+                                    {
+                                        dicDes[item.Key] = item.Value;
+                                    }
+                                    else
+                                    {
+                                        dicDes.Add(item.Key, item.Value);
+                                    }
+                                }
+
+                                var content = JsonConvert.SerializeObject(localization, Formatting.Indented);
+                                File.WriteAllText(desFile, content);
+
+                                //using (FileStream fileStream = new FileStream(desFile, FileMode.Open, FileAccess.Write))
+                                //{
+                                //    byte[] bytes = Encoding.UTF8.GetBytes(content);
+                                //    fileStream.Write(bytes, 0, bytes.Length);
+                                //}
+                            }
+                        }
+                    }
+                }
+
                 "所有代码已经生成完毕，第一次使用可阅读生成的Readme.md文件!".InfoMsg();
             }
             catch (Exception ex)
             {
                 ("代码生成出错! \r\n" + ex.ToString()).ErrorMsg();
             }
+        }
+
+        private class Localization
+        {
+            public string Culture { get; set; }
+
+            public Dictionary<string, string> Texts { get; set; }
         }
 
         private static void AppendFieldCode(this EntityFieldModel entityField, StringBuilder code, bool isEditDto)
@@ -98,38 +170,43 @@ namespace AbpDtoGenerator.LGFeature
                 entityField.FieldTypeStr = entityField.FieldTypeStr.Replace("ICollection<", "List<");
             }
             entityField.FieldTypeStrFirstCharToLower = entityField.FieldTypeStr.FirstCharToLower();
-            if (entityField.MaxLength != null)
+
+            if (isEditDto)
             {
-                string text = entityField.AttributesList.Find((string o) => o.StartsWith("[MaxLength("));
-                if (text != null)
+                if (entityField.MaxLength != null)
                 {
-                    code.AppendLine(text);
+                    string text = entityField.AttributesList.Find((string o) => o.StartsWith("[MaxLength("));
+                    if (text != null)
+                    {
+                        code.AppendLine(text);
+                    }
+                    else
+                    {
+                        code.AppendLine("\t\t[MaxLength({{MaxLength}}, ErrorMessage=\"{{PropAnnotation}}超出最大长度\")]");
+                    }
                 }
-                else
+                if (entityField.MinLength != null)
                 {
-                    code.AppendLine("\t\t[MaxLength({{MaxLength}}, ErrorMessage=\"{{PropAnnotation}}超出最大长度\")]");
+                    string text2 = entityField.AttributesList.Find((string o) => o.StartsWith("[MinLength("));
+                    if (text2 != null)
+                    {
+                        code.AppendLine(text2);
+                    }
+                    else
+                    {
+                        code.AppendLine("\t\t[MinLength({{MinLength}}, ErrorMessage=\"{{PropAnnotation}}小于最小长度\")]");
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(entityField.RegularExpression))
+                {
+                    code.AppendLine("\t\t[RegularExpression(\"{{RegularExpression}}\",ErrorMessage=\"{{PropAnnotation}}格式错误\")]");
+                }
+                if (entityField.Required)
+                {
+                    code.AppendLine("\t\t[Required(ErrorMessage=\"{{PropAnnotation}}不能为空\")]");
                 }
             }
-            if (entityField.MinLength != null)
-            {
-                string text2 = entityField.AttributesList.Find((string o) => o.StartsWith("[MinLength("));
-                if (text2 != null)
-                {
-                    code.AppendLine(text2);
-                }
-                else
-                {
-                    code.AppendLine("\t\t[MinLength({{MinLength}}, ErrorMessage=\"{{PropAnnotation}}小于最小长度\")]");
-                }
-            }
-            if (!string.IsNullOrWhiteSpace(entityField.RegularExpression))
-            {
-                code.AppendLine("\t\t[RegularExpression(\"{{RegularExpression}}\",ErrorMessage=\"{{PropAnnotation}}格式错误\")]");
-            }
-            if (entityField.Required)
-            {
-                code.AppendLine("\t\t[Required(ErrorMessage=\"{{PropAnnotation}}不能为空\")]");
-            }
+
             if (entityField.FieldName == "Id")
             {
                 code.AppendLine("\t\tpublic {{PropType}}? {{PropName}} { get; set; }");
@@ -380,8 +457,8 @@ namespace AbpDtoGenerator.LGFeature
                 list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Application\\Dtos\\EntityEditDto.txt"), buildPath));
                 buildPath = Path.Combine(basePath, entityDir, "Dtos", entity.Name + "ListDto.cs");
                 list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Application\\Dtos\\EntityListDto.txt"), buildPath));
-                buildPath = Path.Combine(basePath, entityDir, "Readme.md");
-                list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Readme.txt"), buildPath));
+                //buildPath = Path.Combine(basePath, entityDir, "Readme.md");
+                //list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Readme.txt"), buildPath));
                 if (option.UseNgZorro)
                 {
                     buildPath = Path.Combine(new string[]
@@ -477,11 +554,17 @@ namespace AbpDtoGenerator.LGFeature
                     list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Domain\\DomainService\\IEntityManager.txt"), buildPath2));
                     buildPath2 = Path.Combine(basePath2, entityDir, "DomainService", entity.Name + "Manager.cs");
                     list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Domain\\DomainService\\EntityManager.txt"), buildPath2));
-                    buildPath2 = Path.Combine(basePath2, entityDir, "Duoyuyan.md");
-                    list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Domain\\Duoyuyan.txt"), buildPath2));
+                    //buildPath2 = Path.Combine(basePath2, entityDir, "Duoyuyan.md");
+                    //list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Domain\\Duoyuyan.txt"), buildPath2));
+
+                    buildPath2 = Path.Combine(templateBasePath, "Localization", entityDir, "zh.txt");
+                    list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Domain\\Localization-zh.txt"), buildPath2));
+
+                    buildPath2 = Path.Combine(templateBasePath, "Localization", entityDir, "en.txt");
+                    list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Domain\\Localization-en.txt"), buildPath2));
                 }
-                buildPath2 = Path.Combine(basePath2, entityDir, "Readme.md");
-                list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Readme.txt"), buildPath2));
+                //buildPath2 = Path.Combine(basePath2, entityDir, "Readme.md");
+                //list.Add(CodeTemplateInfo.Create(CodeTemplateType.Server, Path.Combine(templateBasePath, "LGTemplates\\Server\\Readme.txt"), buildPath2));
             }
 
             /*
---
+++ AbpDtoGenerator.LGFeature/GeneratorCodeServiceExtension.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbpDtoGenerator.GeneratorModels;
+using AbpDtoGenerator.Models;
+
+namespace AbpDtoGenerator.LGFeature
+{
+    public static class GeneratorCodeServiceExtension
+    {
+        public static GeneratorCodeReplaceInfo Create(SolutionInfoModel solutionInfo, BasicOptionCfg option,
+            EntityModel entity)
+        {
+            GeneratorCodeReplaceInfo generatorCodeReplaceInfo = new GeneratorCodeReplaceInfo();
+            generatorCodeReplaceInfo.Multi_Language = new List<TranslatorTextList>();
+            generatorCodeReplaceInfo.SolutionNamespace = solutionInfo.SolutionNamespace;
+            generatorCodeReplaceInfo.CompanyNamespace = solutionInfo.CompanyNamespace;
+            generatorCodeReplaceInfo.BaseClassDtoReplacement = ": " + entity.BaseClassDtoName;
+            if (generatorCodeReplaceInfo.BaseClassDtoReplacement == ": ")
+            {
+                generatorCodeReplaceInfo.BaseClassDtoReplacement = string.Empty;
+            }
+            else if (!string.IsNullOrEmpty(entity.BaseClassNameList))
+            {
+                generatorCodeReplaceInfo.BaseClassDtoReplacement =
+                    generatorCodeReplaceInfo.BaseClassDtoReplacement + "," + entity.BaseClassNameList;
+            }
+
+            if (option.UseDomainManagerCode || option.IsAllGeneratorCode)
+            {
+                generatorCodeReplaceInfo.DomainServicesNamespace = "using " + entity.Namespace + ".DomainService;";
+                generatorCodeReplaceInfo.PrivateIEntityNameManager = string.Concat(new string[]
+                {
+                    "private readonly I",
+                    entity.Name,
+                    "Manager _",
+                    entity.LowerName,
+                    "Manager;"
+                });
+                generatorCodeReplaceInfo.IEntityNameManager = string.Concat(new string[]
+                {
+                    ",I",
+                    entity.Name,
+                    "Manager ",
+                    entity.LowerName,
+                    "Manager"
+                });
+                generatorCodeReplaceInfo.EntityNameManager = string.Concat(new string[]
+                {
+                    " _",
+                    entity.LowerName,
+                    "Manager=",
+                    entity.LowerName,
+                    "Manager;"
+                });
+            }
+
+            if (option.UseDomainAuthorizeCode || option.IsAllGeneratorCode)
+            {
+                generatorCodeReplaceInfo.DomainAuthorizationNamespace = "using " + entity.Namespace + ".Authorization;";
+                generatorCodeReplaceInfo.PermissionNode = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "Node)]"
+                });
+                generatorCodeReplaceInfo.PermissionQuery = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "Query)]"
+                });
+                generatorCodeReplaceInfo.PermissionCreate = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "Create)]"
+                });
+                generatorCodeReplaceInfo.PermissionEdit = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "Edit)]"
+                });
+                generatorCodeReplaceInfo.PermissionDelete = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "Delete)]"
+                });
+                generatorCodeReplaceInfo.PermissionBatchDelete = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "BatchDelete)]"
+                });
+                generatorCodeReplaceInfo.PermissionExportExcel = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "ExportExcel)]"
+                });
+                generatorCodeReplaceInfo.EntityCreateOrEditPermission = string.Concat(new string[]
+                {
+                    "[AbpAuthorize(",
+                    entity.Name,
+                    "Permissions.",
+                    "Create, ",
+                    entity.Name,
+                    "Permissions.",
+                    "Edit)]"
+                });
+            }
+
+            return generatorCodeReplaceInfo;
+        }
+    }
+}
